ed2f822d697664665c5b908807bf20ebf9cc9469
sweetkubuni <justinsweng@gmail.com>
2024-04-13 20:45:04 -0400

    add new files for the logic

---
+++ battle_logic.py
@@ -0,0 +1,4 @@
+
+
+#write a main loop that runs the ai logic, the event queue processing, and also captures
+#users input. Finally, it should report all event actions
---
+++ config_characters.py
@@ -0,0 +1,2 @@
+from .jprg import Person
+# we use the Person DataClass from jrpg.py to create example NPCS and PCS
