b383822226d04e23eba5b5e17efea207e81ff894
sweetkubuni <justinsweng@gmail.com>
2024-04-22 14:13:58 -0400

    add comments for battle logic

--- battle_logic.py
+++ battle_logic.py
@@ -1,4 +1,14 @@
 
 
-#write a main loop that runs the ai logic, the event queue processing, and also captures
-#users input. Finally, it should report all event actions
+#params might need to be enemies: List[Person], allies: List[Person]
+
+
+main_loop:
+    #(1) calculate the turn-order of each person
+    #(2) enemy ai and human pre-empthaically selects moves for each person
+    # in their party list
+    #(3) events play out in processing_queue_event function
+    #(4) if main character moral is 0 or  main character (hp is 0) and other
+    #allies hp or moral is zero: you lose. if all enemies either have 0 moral or hp
+    #then you win.
+    # if no winners, go back to (1)
